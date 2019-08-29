46d8770f68c8980779e78c7ee31792b0920d1159
Gianmattia <giamalfred@yahoo.it>
2019-08-30 00:23:00 +0200

    First commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	public GameObject NewHumanWithHat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		int enemiesCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+		if (Time.time > 5 && enemiesCount < 1)
+		{
+			//var newPrefab = Instantiate(NewHumanWithHat);
+			GameObject prefab = Resources.Load(@"Prefabs/Human") as GameObject;
+			Instantiate(prefab);
+		}		
+	}
+}
