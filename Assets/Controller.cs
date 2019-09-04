40d497d668a1600ee345abadf62fe216ea087ab4
Gianmattia <giamalfred@yahoo.it>
2019-09-05 00:18:03 +0200

    More Tests

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
 	public GameObject NewHumanWithHat;
+	private List<PlayerAction> PlayerActions;
+	private readonly Func<PlayerAction, bool> VerifyInput = (p) => { return Input.GetKey(p.AssignedKey); };
 
     // Start is called before the first frame update
     void Start()
     {
-    }
+		PlayerActions = new List<PlayerAction>()
+		{
+			new PlayerAction("MoveForward", KeyCode.UpArrow, MovingUp),
+			new PlayerAction("MouseClick", KeyCode.Mouse0, MouseClick)
+		};
+	}
 
     // Update is called once per frame
     void Update()
@@ -20,6 +29,33 @@ public class Controller : MonoBehaviour
 			//var newPrefab = Instantiate(NewHumanWithHat);
 			GameObject prefab = Resources.Load(@"Prefabs/Human") as GameObject;
 			Instantiate(prefab);
-		}		
+		}
+
+		IEnumerable<PlayerAction> inputtedPlayerActions = PlayerActions.Where(VerifyInput);
+		foreach (PlayerAction inputtedPlayerAction in inputtedPlayerActions)
+		{
+			inputtedPlayerAction.Action();
+		}
+		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(mouseRay))
+		{
+			Debug.Log("Mouse hit something!");
+		}
+	}
+
+	void MovingUp() { Debug.Log("Moving up!"); }
+	void MouseClick() { Debug.Log("Click!"); }
+}
+
+public class PlayerAction
+{
+	public string Name { get; set; }
+	public KeyCode AssignedKey { get; set; }
+	public Action Action { get; set; }
+	public PlayerAction(string name, KeyCode assignedKey, Action action)
+	{
+		this.Name = name;
+		this.AssignedKey = assignedKey;
+		this.Action = action;
 	}
 }
--- Assets/HumanWithHatBehaviour.cs
+++ Assets/HumanWithHatBehaviour.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class HumanWithHatBehaviour : MonoBehaviour
 {
 	GameObject Enemy;
-	Rigidbody Body;
+	GameObject Body;
 	Ray Ray;
 	// Start is called before the first frame update
 	void Start()
     {
 		Debug.Log(gameObject.name);
-		Body = gameObject.GetComponent<Rigidbody>();
+		Body = gameObject.transform.Find("Body")?.gameObject;
 		Enemy = GameObject.FindWithTag("Enemy");
 		//Rigidbody newBody = gameObject.AddComponent<Rigidbody>();
 		//newBody.drag = 0.1f;
@@ -21,29 +21,33 @@ public class HumanWithHatBehaviour : MonoBehaviour
     void Update()
     {
 		//Debug.Log($"Position: {gameObject.transform.position.y}");
-		if (Body && Body.useGravity && gameObject.transform.position.y < -1)
-		{
-			Body.useGravity = false;
-			Body.velocity = new Vector3(0, 0, 0);
-			Body.drag = 0;
-			Body.angularDrag = 0;
-		}
+		//if (Body && Body.useGravity && gameObject.transform.position.y < -1)
+		//{
+		//	Body.useGravity = false;
+		//	Body.velocity = new Vector3(0, 0, 0);
+		//	Body.drag = 0;
+		//	Body.angularDrag = 0;
+		//}
 	}
 
 	private void FixedUpdate()
 	{
-		if (Enemy != null && Time.time > 2)
+		if (!Enemy)
 		{
-			Ray = new Ray(new Vector3(Body.transform.position.x, Body.transform.position.y + 1, Body.transform.position.z), new Vector3(Enemy.transform.position.x, Enemy.transform.position.y + 1, Enemy.transform.position.z));			
+			Enemy = GameObject.FindWithTag("Enemy");
+		}		
+		if (Enemy != null)
+		{
+			Ray = new Ray(Body.transform.position, Vector3.forward);			
 			RaycastHit hit;
-			Debug.Log($"Trying to hit: {Enemy.name} at position: {Enemy.transform.position}");
-			Debug.DrawLine(Body.position, Vector3.forward, Color.red, 2);
-			Debug.DrawLine(Ray.origin, Ray.direction, Color.red, 1);
+			//Debug.Log($"Trying to hit: {Enemy.name} at position: {Enemy.transform.position}");
+			Debug.DrawRay(Ray.origin, Ray.direction, Color.red, 1);
 			if (Physics.Raycast(Ray, out hit, Mathf.Infinity))
 			{
-				Debug.Log($"Enemy hit! {hit.transform.name}");
-				Destroy(Enemy);
-				Enemy = GameObject.FindWithTag("Enemy");
+				Debug.Log($"Enemy hit! {hit.collider.name}");
+				GameObject hitRender = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				hitRender.transform.position = hit.transform.position;
+				Destroy(Enemy);				
 			}
 		}
 	}
